d9fe338b36f1f92921ed995dfc8afeae3fec4e62
Artchibald <archie@archibaldbutler.com>
2022-11-28 15:10:13 +0000

    all working, just need to set up exports correctly in svg and png

--- latest.ts
+++ latest.ts
@@ -13,20 +13,6 @@ let CSTasks = (function () {
   return rect;
  };
 
-
- //takes a group
- //ungroups that group at the top layer (no recursion for nested groups)
- tasks.ungroupOnce = function (group) {
-  for (i = group.pageItems.length - 1; i >= 0; i--) {
-   group.pageItems[i].move(
-    group.pageItems[i].layer,
-    /*@ts-ignore*/
-    ElementPlacement.PLACEATEND
-   );
-  }
- };
-
-
  tasks.selectContentsOnArtboard = function (sourceDoc, i) {
   sourceDoc.selection = null;
   sourceDoc.artboards.setActiveArtboardIndex(i);
@@ -61,39 +47,6 @@ let CSTasks = (function () {
  tasks.translateObjectTo = function (object, destination) {
   let offset = tasks.getOffset(object.position, destination);
   object.translate(-offset[0], -offset[1]);
- };
-
- /****************************
-   CREATING AND SAVING DOCUMENTS
-   *****************************/
-
- //take a source document and a colorspace (e.g. DocumentColorSpace.RGB)
- //opens and returns a new document with the source document's units and the specified colorspace
- tasks.newDocument = function (sourceDoc, colorSpace) {
-  let preset = new DocumentPreset();
-  /*@ts-ignore*/
-  preset.colorMode = colorSpace;
-  /*@ts-ignore*/
-  preset.units = sourceDoc.rulerUnits;
-  /*@ts-ignore*/
-  let newDoc = app.documents.addDocument(colorSpace, preset);
-  newDoc.pageOrigin = sourceDoc.pageOrigin;
-  newDoc.rulerOrigin = sourceDoc.rulerOrigin;
-  return newDoc;
- };
-
- //take a source document, artboard index, and a colorspace (e.g. DocumentColorSpace.RGB)
- //opens and returns a new document with the source document's units and specified artboard, the specified colorspace
- tasks.duplicateArtboardInNewDoc = function (
-  sourceDoc,
-  artboardIndex,
-  colorspace
- ) {
-  let rectToCopy = sourceDoc.artboards[artboardIndex].artboardRect;
-  let newDoc = tasks.newDocument(sourceDoc, colorspace);
-  newDoc.artboards.add(rectToCopy);
-  newDoc.artboards.remove(0);
-  return newDoc;
  };
 
 
@@ -148,19 +101,19 @@ function process(files) {
   //alert(sourceDoc.name);
 
   // create 800x500 artboard in file
-  // let FifthMainArtboardFirstRect = sourceDoc.artboards[1].artboardRect;
-  // sourceDoc.artboards.add(
-  //  // this fires but then gets replaced further down
-  //  CSTasks.newRect(FifthMainArtboardFirstRect[1], 370, 800, 500)
-  // );
+  let ThirdMainArtboardFirstRect = sourceDoc.artboards[1].artboardRect;
+  sourceDoc.artboards.add(
+   // this fires but then gets replaced further down
+   CSTasks.newRect(ThirdMainArtboardFirstRect[1], 370, 800, 500)
+  );
 
   //select the contents on artboard 1
-  let selFifthBanner = CSTasks.selectContentsOnArtboard(sourceDoc, 1);
+  let selThirdBanner = CSTasks.selectContentsOnArtboard(sourceDoc, 1);
 
   // make sure all colors are RGB, equivalent of Edit > Colors > Convert to RGB
   app.executeMenuCommand('Colors9');
 
-  if (selFifthBanner.length == 0) {
+  if (selThirdBanner.length == 0) {
    //if nothing is in the artboard
    alert("Please try again with artwork on the main second 256x256 artboard.");
    return;
@@ -169,7 +122,7 @@ function process(files) {
   /********************************
   Add elements to new fourth artboard with lockup
   *********************************/
-  let iconGroup = CSTasks.createGroup(sourceDoc, selFifthBanner); //group the selection (easier to work with)
+  let iconGroup = CSTasks.createGroup(sourceDoc, selThirdBanner); //group the selection (easier to work with)
   let iconOffset = CSTasks.getOffset(
    iconGroup.position,
    CSTasks.getArtboardCorner(sourceDoc.artboards[1])
@@ -178,230 +131,100 @@ function process(files) {
 
   //place icon on lockup
   /*@ts-ignore*/
-  let fourthBannerMast = iconGroup.duplicate(iconGroup.layer, ElementPlacement.PLACEATEND);
-  let fourthBannerMastPos = [
+  let thirdBannerMast = iconGroup.duplicate(iconGroup.layer, ElementPlacement.PLACEATEND);
+  let thirdBannerMastPos = [
    sourceDoc.artboards[2].artboardRect[0] + iconOffset[0],
    sourceDoc.artboards[2].artboardRect[1] + iconOffset[1],
   ];
-  CSTasks.translateObjectTo(fourthBannerMast, fourthBannerMastPos);
+  CSTasks.translateObjectTo(thirdBannerMast, thirdBannerMastPos);
 
   let getArtLayer5 = sourceDoc.layers.getByName('Art');
-  let landingZoneSquare5 = getArtLayer5.pathItems.rectangle(
+  let landingZoneSquare = getArtLayer5.pathItems.rectangle(
    -2024,
    352,
    456,
    456);
 
-  function placeIconLockup1Correctly5(fourthBannerMast, maxSize) {
+  function placeIconLockup1Correctly5(thirdBannerMast, maxSize) {
    // let setLandingZoneSquareColor = new RGBColor();
    // setLandingZoneSquareColor.red = 121;
    // setLandingZoneSquareColor.green = 128;
    // setLandingZoneSquareColor.blue = 131;
-   // landingZoneSquare5.fillColor = setLandingZoneSquareColor;
-   landingZoneSquare5.name = "LandingZone2"
-   landingZoneSquare5.filled = false;
+   // landingZoneSquare.fillColor = setLandingZoneSquareColor;
+   landingZoneSquare.name = "LandingZone2"
+   landingZoneSquare.filled = false;
    /*@ts-ignore*/
-   landingZoneSquare5.move(getArtLayer5, ElementPlacement.PLACEATEND);
+   landingZoneSquare.move(getArtLayer5, ElementPlacement.PLACEATEND);
 
    // start moving expressive icon into our new square landing zone
-   let placedfourthBannerMast = fourthBannerMast;
+   let placedthirdBannerMast = thirdBannerMast;
    let landingZone = sourceDoc.pathItems.getByName("LandingZone2");
    let preferredWidth = (456);
    let preferredHeight = (456);
    // do the width
-   let widthRatio = (preferredWidth / placedfourthBannerMast.width) * 100;
-   if (placedfourthBannerMast.width != preferredWidth) {
-    placedfourthBannerMast.resize(widthRatio, widthRatio);
+   let widthRatio = (preferredWidth / placedthirdBannerMast.width) * 100;
+   if (placedthirdBannerMast.width != preferredWidth) {
+    placedthirdBannerMast.resize(widthRatio, widthRatio);
    }
    // now do the height
-   let heightRatio = (preferredHeight / placedfourthBannerMast.height) * 100;
-   if (placedfourthBannerMast.height != preferredHeight) {
-    placedfourthBannerMast.resize(heightRatio, heightRatio);
+   let heightRatio = (preferredHeight / placedthirdBannerMast.height) * 100;
+   if (placedthirdBannerMast.height != preferredHeight) {
+    placedthirdBannerMast.resize(heightRatio, heightRatio);
    }
    // now let's center the art on the landing zone
-   let centerArt = [placedfourthBannerMast.left + (placedfourthBannerMast.width / 2), placedfourthBannerMast.top + (placedfourthBannerMast.height / 2)];
+   let centerArt = [placedthirdBannerMast.left + (placedthirdBannerMast.width / 2), placedthirdBannerMast.top + (placedthirdBannerMast.height / 2)];
    let centerLz = [landingZone.left + (landingZone.width / 2), landingZone.top + (landingZone.height / 2)];
-   placedfourthBannerMast.translate(centerLz[0] - centerArt[0], centerLz[1] - centerArt[1]);
+   placedthirdBannerMast.translate(centerLz[0] - centerArt[0], centerLz[1] - centerArt[1]);
 
    // need another centered proportioning to fix it exactly in correct position
-   let W = fourthBannerMast.width,
-    H = fourthBannerMast.height,
+   let W = thirdBannerMast.width,
+    H = thirdBannerMast.height,
     MW = maxSize.W,
     MH = maxSize.H,
     factor = W / H > MW / MH ? MW / W * 100 : MH / H * 100;
-   fourthBannerMast.resize(factor, factor);
+   thirdBannerMast.resize(factor, factor);
   }
-  placeIconLockup1Correctly5(fourthBannerMast, { W: 456, H: 456 });
+  placeIconLockup1Correctly5(thirdBannerMast, { W: 456, H: 456 });
 
   // delete the landing zone
-  landingZoneSquare5.remove();
+  landingZoneSquare.remove();
 
 
+
+
+  // thirdBannerMast.width = 456;
+  // thirdBannerMast.height = 456;
 
   // new purple bg
   // Add new layer above Guidelines and fill white
-  let fifthMainArtworkLayer = sourceDoc.layers.getByName('Art');
-  let fifthMainPurpleBgLayer = sourceDoc.layers.add();
-  fifthMainPurpleBgLayer.name = "Main_Purple_BG_layer_two";
-  let getfifthMainPurpleBgLayer = sourceDoc.layers.getByName('Main_Purple_BG_layer_two');
-  let fifthMainRect = getfifthMainPurpleBgLayer.pathItems.rectangle(
+  let thirdMainArtworkLayer = sourceDoc.layers.getByName('Art');
+  let thirdMainPurpleBgLayer = sourceDoc.layers.add();
+  thirdMainPurpleBgLayer.name = "Main_Purple_BG_layer_two";
+  let getthirdMainPurpleBgLayer = sourceDoc.layers.getByName('Main_Purple_BG_layer_two');
+  let thirdMainRect = getthirdMainPurpleBgLayer.pathItems.rectangle(
    -1972,
    0,
    800,
    500);
-  let fifthMainVioletBgColor = new RGBColor();
-  fifthMainVioletBgColor.red = 72;
-  fifthMainVioletBgColor.green = 8;
-  fifthMainVioletBgColor.blue = 111;
-  fifthMainRect.filled = true;
-  fifthMainRect.fillColor = fifthMainVioletBgColor;
+  let thirdMainVioletBgColor = new RGBColor();
+  thirdMainVioletBgColor.red = 72;
+  thirdMainVioletBgColor.green = 8;
+  thirdMainVioletBgColor.blue = 111;
+  thirdMainRect.filled = true;
+  thirdMainRect.fillColor = thirdMainVioletBgColor;
   /*@ts-ignore*/
-  getfifthMainPurpleBgLayer.move(fifthMainArtworkLayer, ElementPlacement.PLACEATEND);
+  getthirdMainPurpleBgLayer.move(thirdMainArtworkLayer, ElementPlacement.PLACEATEND);
 
   /*@ts-ignore*/
   // svgFile.embed();
-  // let fifthResizedRect = CSTasks.newRect(
-  //    sourceDoc.artboards[5].artboardRect[0],
-  //    -sourceDoc.artboards[5].artboardRect[1],
-  //    800,
-  //    500
-  // );
-  // sourceDoc.artboards[5].artboardRect = fifthResizedRect;
-
-  /********************
-  Purple fifth Lockup with no text export at 800x500
-  ********************/
-  //open a new doc and copy and position the icon
-  // duplication did not work as expected here. I have used a less elegant solution whereby I recreated the purple banner instead of copying it.
-  let mastDocNoText800x500 = CSTasks.duplicateArtboardInNewDoc(
-   sourceDoc,
-   2,
-   DocumentColorSpace.RGB
-  );
-  mastDocNoText800x500.swatches.removeAll();
-
-  let mastGroupNoText800x500 = iconGroup.duplicate(
-   mastDocNoText800x500.layers[0],
-   /*@ts-ignore*/
-   ElementPlacement.PLACEATEND
-  );
-  // new icon width in rebrand
-  // mastGroupNoText800x500.width = 360;
-  // mastGroupNoText800x500.height = 360;
-
-  // new icon position
-  let mastLocNoText800x500 = [
-   mastDocNoText800x500.artboards[0].artboardRect[0],
-   mastDocNoText800x500.artboards[0].artboardRect[1],
-  ];
-  CSTasks.translateObjectTo(mastGroupNoText800x500, mastLocNoText800x500);
-
-
-  /********************************
-    Custom function to create a landing square to place the icon correctly
-    Some icons have width or height less than 256 so it needed special centering geometrically
-    you can see the landing zone square by changing fill to true and uncommenting color
-    *********************************/
-
-  // create a landing zone square to place icon inside
-  //moved it outside the function itself so we can delete it after so it doesn't get exported
-  let getArtLayerIn5thArtboard = mastDocNoText800x500.layers.getByName('Layer 1');
-  let landingZoneSquareInFifthArtboard = getArtLayerIn5thArtboard.pathItems.rectangle(
-   -2022,
-   352,
-   456,
-   456);
-
-  function placeIconLockupCorrectlyIn5thDoc(mastGroupNoText800x500, maxSize) {
-
-   // let setLandingZoneSquareColor = new RGBColor();
-   // setLandingZoneSquareColor.red = 121;
-   // setLandingZoneSquareColor.green = 128;
-   // setLandingZoneSquareColor.blue = 131;
-   // landingZoneSquareInFifthArtboard.fillColor = setLandingZoneSquareColor;
-
-   landingZoneSquareInFifthArtboard.name = "LandingZone4"
-   landingZoneSquareInFifthArtboard.filled = false;
-   /*@ts-ignore*/
-   landingZoneSquareInFifthArtboard.move(getArtLayerIn5thArtboard, ElementPlacement.PLACEATEND);
-
-   // start moving expressive icon into our new square landing zone
-   let placedmastGroup = mastGroupNoText800x500;
-   let landingZone = mastDocNoText800x500.pathItems.getByName("LandingZone4");
-   let preferredWidth = (456);
-   let preferredHeight = (456);
-   // do the width
-   let widthRatio = (preferredWidth / placedmastGroup.width) * 100;
-   if (placedmastGroup.width != preferredWidth) {
-    placedmastGroup.resize(widthRatio, widthRatio);
-   }
-   // now do the height
-   let heightRatio = (preferredHeight / placedmastGroup.height) * 100;
-   if (placedmastGroup.height != preferredHeight) {
-    placedmastGroup.resize(heightRatio, heightRatio);
-   }
-   // now let's center the art on the landing zone
-   let centerArt = [placedmastGroup.left + (placedmastGroup.width / 2), placedmastGroup.top + (placedmastGroup.height / 2)];
-   let centerLz = [landingZone.left + (landingZone.width / 2), landingZone.top + (landingZone.height / 2)];
-   placedmastGroup.translate(centerLz[0] - centerArt[0], centerLz[1] - centerArt[1]);
-
-   // need another centered proportioning to fix it exactly in correct position
-   let W = mastGroupNoText800x500.width,
-    H = mastGroupNoText800x500.height,
-    MW = maxSize.W,
-    MH = maxSize.H,
-    factor = W / H > MW / MH ? MW / W * 100 : MH / H * 100;
-   mastGroupNoText800x500.resize(factor, factor);
-  }
-  placeIconLockupCorrectlyIn5thDoc(mastGroupNoText800x500, { W: 456, H: 456 });
-
-  // delete the landing zone
-  landingZoneSquareInFifthArtboard.remove();
-
-  CSTasks.ungroupOnce(mastGroupNoText800x500);
-
-  // add new style purple banner 4 elements
-  let myMainArtworkLayerMastDocNoText800x500 = mastDocNoText800x500.layers.getByName('Layer 1');
-  let myMainPurpleBgLayerMastDocNoText800x500 = mastDocNoText800x500.layers.add();
-  myMainPurpleBgLayerMastDocNoText800x500.name = "Main_Purple_BG_layer";
-  let GetMyMainPurpleBgLayerMastDocNoText800x500 = mastDocNoText800x500.layers.getByName('Main_Purple_BG_layer');
-  let mainRectMastDocNoText800x500 = GetMyMainPurpleBgLayerMastDocNoText800x500.pathItems.rectangle(
-   -1972,
-   0,
+  let thirdResizedRect = CSTasks.newRect(
+   sourceDoc.artboards[2].artboardRect[0],
+   -sourceDoc.artboards[2].artboardRect[1],
    800,
-   500);
-  let setMainVioletBgColorMastDocNoText800x500 = new RGBColor();
-  setMainVioletBgColorMastDocNoText800x500.red = 72;
-  setMainVioletBgColorMastDocNoText800x500.green = 8;
-  setMainVioletBgColorMastDocNoText800x500.blue = 111;
-  mainRectMastDocNoText800x500.filled = true;
-  mainRectMastDocNoText800x500.fillColor = setMainVioletBgColorMastDocNoText800x500;
-  /*@ts-ignore*/
-  GetMyMainPurpleBgLayerMastDocNoText800x500.move(myMainArtworkLayerMastDocNoText800x500, ElementPlacement.PLACEATEND);
+   500
+  );
+  sourceDoc.artboards[2].artboardRect = thirdResizedRect;
 
-  // we need to make artboard clipping mask here for the artboard to crop expressive icons correctly.
-  let myCroppingLayerMastDocNoText800x500 = mastDocNoText800x500.layers.add();
-  myCroppingLayerMastDocNoText800x500.name = "crop";
-  let GetMyCroppingLayerMastDocNoText800x500 = mastDocNoText800x500.layers.getByName('crop');
-  mastDocNoText800x500.activeLayer = GetMyCroppingLayerMastDocNoText800x500;
-  mastDocNoText800x500.activeLayer.hasSelectedArtwork = true;
-  // insert clipping rect here
-  let mainClipRectMastDocNoText800x500 = GetMyCroppingLayerMastDocNoText800x500.pathItems.rectangle(
-   -1972,
-   0,
-   800,
-   500);
-  // let setClipBgColorMastDocNoText800x500 = new RGBColor();
-  // setClipBgColorMastDocNoText800x500.red = 111;
-  // setClipBgColorMastDocNoText800x500.green = 111;
-  // setClipBgColorMastDocNoText800x500.blue = 222;
-  // mainClipRectMastDocNoText800x500.filled = true;
-  // mainClipRectMastDocNoText800x500.fillColor = setClipBgColorMastDocNoText800x500;
-  // select all for clipping here
-  mastDocNoText800x500.selectObjectsOnActiveArtboard();
-
-  // clip!
-  app.executeMenuCommand('makeMask');
 
   return;
   // ends here
